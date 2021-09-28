0549251e50dcdd291e4cf82594c80796548f25ae
kael <fengzeyan>
2021-09-28 12:57:27 +0800

    add some content

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="https://avatars.githubusercontent.com/u/1179735?v=4">
+    <img alt="Vue logo" src="http://wx2.sinaimg.cn/mw1024/007QHGQRly1guqxxo6dt6g607s08g1kz02.gif">
     <HelloWorld msg="狗东西们，Morning Call 了吗"/>
   </div>
 </template>
